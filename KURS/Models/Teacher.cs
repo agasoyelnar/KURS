55fa807b1df54380269d64713dfa7d92a022b469
hasanaghaver <hasanaa-apa202@code.edu.az>
2026-03-16 13:59:05 +0400

    Tst

---
+++ KURS/Models/Teacher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace KURS
+{
+
+
+    class Teacher
+    {
+
+        
+        private int id;
+        private string name;
+        private string surname;
+        private string subject;
+        private string experience;
+        private string groupNumber;
+        private DateTime birthDate;
+        private string address;
+        private string phoneNumber;
+        private string email;
+        private string username;
+
+
+
+
+
+
+
+        
+        public string Name { get { return name; } private set { name = value; } }
+        public string Surname { get { return surname; } private set { surname = value; } }
+        public string Subject { get { return subject; } private set { subject = value; } }
+        public string Username { get { return username; } private set { username = value; } }
+        public string GroupNumber { get { return groupNumber; } private set { groupNumber = value; } }
+        public DateTime BirthDate { get { return birthDate; } private set { birthDate = value; } }
+        public string Address { get { return address; } private set { address = value; } }
+        public string PhoneNumber { get { return phoneNumber; } private set { phoneNumber = value; } }
+        public string Email { get { return email; } private set { email = value; } }
+
+        
+        public Teacher(int id, string name, string surname, string subject, string experience,
+            string groupNumber, DateTime birthDate, string address, string phoneNumber, string email, string username)
+        {
+
+
+            this.id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.Subject = subject;
+            this.username = username;
+            this.GroupNumber = groupNumber;
+            this.BirthDate = birthDate;
+            this.Address = address;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
--- KURS/Program.cs
+++ KURS/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace KURS
+﻿using System;
+
+namespace KURS
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Teacher teacher1 = new Teacher(
+                1,
+                "Jale",
+                "Mammadova",
+                "C#",
+                "mid level programmer",
+                "BP101",
+                new DateTime(1990, 5, 10),
+                "Baku",
+                "0501234567",
+                "ali@gmail.com",
+                "12345"
+            );
+
+            Console.WriteLine("Teacher Name: " + teacher1.Name);
+            Console.WriteLine("Surname: " + teacher1.Surname);
+            Console.WriteLine("Subject: " + teacher1.Subject);
+            Console.WriteLine("Username: " + teacher1.Username);
+            Console.WriteLine("Group Number: " + teacher1.GroupNumber);
+            Console.WriteLine("Birth Date: " + teacher1.BirthDate.ToShortDateString());
+            Console.WriteLine("Address: " + teacher1.Address);
+            Console.WriteLine("Phone: " + teacher1.PhoneNumber);
+            Console.WriteLine("Email: " + teacher1.Email);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
