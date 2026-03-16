e9c5dd5a9da0c37d520cd13a358be6e74be24b0c
Huseynov55 <anarchh-apa202@code.edu.az>
2026-03-16 13:47:43 +0400

    task2

--- KURS/Program.cs
+++ KURS/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace KURS
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using KURS.Models;
+
+Student telebe = new Student(
+    "huseynov55",
+    "Hüseyn",
+    "Hüseynov",
+    101,
+    2000,
+    "Bakı ş.",
+    "+994550000000",
+    "huseyn@mail.com",
+    "pass123",
+    95
+);
+
+Console.WriteLine(telebe.GetName());
+Console.WriteLine(telebe.GetGroup());
+Console.WriteLine(telebe.GetStudent());
+Console.WriteLine(telebe.GetEmail());
+Console.WriteLine(telebe.GetBirthDay());
+Console.WriteLine(telebe.GetContact());
